8a2580839038380168be580fd872f460132fcd20
zhulongpeng <404859467@qq.com>
2019-08-06 23:16:10 +0800

    d

--- Shopping-admin/src/main/java/com/zlp/admin/dto/UmsAdminLoginParam.java
+++ Shopping-admin/src/main/java/com/zlp/admin/dto/UmsAdminLoginParam.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class UmsAdminLoginParam {
+public class UmsAdminLoginParam implements Serializable {
 
     @ApiModelProperty(value = "用户名", required = true)
     @NotEmpty(message = "用户名不能为空")
@@ -17,4 +19,19 @@ public class UmsAdminLoginParam {
     @NotEmpty(message = "密码不能为空")
     private String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- Shopping-admin/src/main/java/com/zlp/admin/dto/UmsAdminParam.java
+++ Shopping-admin/src/main/java/com/zlp/admin/dto/UmsAdminParam.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class UmsAdminParam {
+public class UmsAdminParam implements Serializable {
 
     @ApiModelProperty(value = "用户名", required = true)
     @NotEmpty(message = "用户名不能为空")
@@ -30,4 +32,52 @@ public class UmsAdminParam {
 
     @ApiModelProperty(value = "备注")
     private String note;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
